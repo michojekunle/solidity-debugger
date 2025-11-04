b5d003d83c304e586df1a09073c8d2f82f241b3c
AMD <michojekunle1@gmail.com>
2025-11-04 09:30:10 +0100

    starting from the ground up

--- gas-analyzer-extension/src/extension.ts
+++ gas-analyzer-extension/src/extension.ts
@@ -1,40 +1,41 @@
 // src/extension.ts
-import * as vscode from 'vscode';
-import { StaticAnalyzer } from './analyzers/staticAnalyzer';
-import { HoverProvider } from './providers/hoverProvider';
-import { DiagnosticProvider } from './providers/diagnosticProvider';
+import * as vscode from "vscode";
+import { StaticAnalyzer } from "./analyzers/staticAnalyzer";
+import { HoverProvider } from "./providers/hoverProvider";
+import { DiagnosticProvider } from "./providers/diagnosticProvider";
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Gas Optimizer Extension activated');
-    
-    // Initialize analyzers
-    const staticAnalyzer = new StaticAnalyzer();
-    const diagnosticProvider = new DiagnosticProvider(staticAnalyzer);
-    
-    // Register providers
-    const hoverProvider = vscode.languages.registerHoverProvider(
-        'solidity', 
-        new HoverProvider(staticAnalyzer)
-    );
-    
-    const diagnosticCollection = vscode.languages.createDiagnosticCollection('gasOptimizer');
-    
-    // File watcher for real-time analysis
-    const fileWatcher = vscode.workspace.onDidChangeTextDocument(
-        debounce((e: any) => analyzeDocument(e.document), 500)
-    );
-    
-    context.subscriptions.push(hoverProvider, fileWatcher);
+  console.log("Gas Optimizer Extension activated");
+
+  // Initialize analyzers
+  const staticAnalyzer = new StaticAnalyzer();
+  const diagnosticProvider = new DiagnosticProvider(staticAnalyzer);
+
+  // Register providers
+  const hoverProvider = vscode.languages.registerHoverProvider(
+    "solidity",
+    new HoverProvider(staticAnalyzer)
+  );
+
+  const diagnosticCollection =
+    vscode.languages.createDiagnosticCollection("gasOptimizer");
+
+  // File watcher for real-time analysis
+  const fileWatcher = vscode.workspace.onDidChangeTextDocument(
+    debounce((e) => analyzeDocument(e.document), 500)
+  );
+
+  context.subscriptions.push(hoverProvider, fileWatcher);
 }
 
 function debounce(func: Function, wait: number) {
-    let timeout: NodeJS.Timeout;
-    return function executedFunction(...args: any[]) {
-        const later = () => {
-            clearTimeout(timeout);
-            func(...args);
-        };
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
+  let timeout: NodeJS.Timeout;
+  return function executedFunction(...args: any[]) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
     };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
 }
